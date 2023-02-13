9a1aad1d7d3debf598f38c6d4ff97a50361fb02b
Obihan98 <orhan.ozbasaran@outlook.com>
2023-02-13 09:52:36 -0800

    Navbar - completed

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,48 @@
+import { NavLink } from "react-router-dom";
+import React, {useState} from 'react';
 import "./index.css"
 
 
 const Navbar = () => {
+    const[Toggle, setToggle] = useState(true);
+
     return <>
-        Navbar is here!
+    <header className='header'>
+        <nav className='nav container'>
+            <NavLink exact='True' className={isActive =>
+                "nav__logo" + (!isActive ? " unselected" : "")
+                } to='/'>Chocoland</NavLink>
+
+            <div className={Toggle ? "nav__menu show-menu" : "nav__menu"}>
+                <ul className='nav__list'>
+                    <li className='nav__item'>
+                        <NavLink exact='True' className='nav__link' activeclassname='selected' to='/'>
+                            <i className='uil uil-estate nav__icon'></i>
+                            Home
+                        </NavLink>
+                    </li>
+                    <li className='nav__item'>
+                        <NavLink exact='True' className='nav__link' activeclassname='selected' to='/Products'>
+                            <i className='uil uil-user nav__icon'></i>
+                            Products
+                        </NavLink>
+                    </li>
+                    <li className='nav__item'>
+                        <NavLink exact='True' className='nav__link' activeclassname='selected' to='/Cart'>
+                            <i className='uil uil-file-alt nav__icon'></i>
+                            Cart
+                        </NavLink>
+                    </li>                  
+                </ul>
+
+                <i className='uil uil-times nav__close' onClick={() => setToggle(!Toggle)}></i>
+            </div>
+
+            <div className='nav__toggle' onClick={() => setToggle(!Toggle)}>
+                <i className='uil uil-apps'></i>
+            </div>
+        </nav>
+    </header>  
     </>
 }
 
