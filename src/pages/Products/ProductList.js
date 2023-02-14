1817495bdc8c0923f8d7af975e8403d75460d72f
Obihan98 <orhan.ozbasaran@outlook.com>
2023-02-14 10:34:45 -0800

    ProductList updated

---
+++ src/pages/Products/ProductList.js
@@ -0,0 +1,151 @@
+import "./index.css"
+import Kinder from "../../assets/images/kinder.jpeg"
+
+const ProductList = () => {
+    
+   
+    return <>
+        <section class="new" id="new">
+            <div class="centered-text">
+                <h2>White</h2>
+            </div>
+
+            <div class="new-content">
+                <div class="box">
+                    <img src={Kinder}></img>
+                    <h5>Lorem</h5>
+                    <h6>$188</h6> 
+                    <div class="sale">
+                        <h4>Sale</h4>
+                    </div>
+                </div>
+
+
+                <div class="box">
+                <img src={Kinder}></img>
+                    <h5>Lorem</h5>
+                    <h6>$188</h6> 
+                    <div class="sale">
+                        <h4>Sale</h4>
+                    </div>
+                </div>
+
+
+                <div class="box">
+                    <img src={Kinder}></img>
+                    <h5>Lorem</h5>
+                    <h6>$188</h6> 
+                    <div class="sale">
+                        <h4>Sale</h4>
+                    </div>
+                </div>
+
+
+                <div class="box">
+                    <img src={Kinder}></img>
+                    <h5>Lorem</h5>
+                    <h6>$188</h6> 
+                    <div class="sale">
+                        <h4>Sale</h4>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="new" id="new">
+            <div class="centered-text">
+                <h2>Dark</h2>
+            </div>
+
+            <div class="new-content">
+                <div class="box">
+                    <img src={Kinder}></img>
+                    <h5>Lorem</h5>
+                    <h6>$188</h6> 
+                    <div class="sale">
+                        <h4>Sale</h4>
+                    </div>
+                </div>
+
+
+                <div class="box">
+                <img src={Kinder}></img>
+                    <h5>Lorem</h5>
+                    <h6>$188</h6> 
+                    <div class="sale">
+                        <h4>Sale</h4>
+                    </div>
+                </div>
+
+
+                <div class="box">
+                    <img src={Kinder}></img>
+                    <h5>Lorem</h5>
+                    <h6>$188</h6> 
+                    <div class="sale">
+                        <h4>Sale</h4>
+                    </div>
+                </div>
+
+
+                <div class="box">
+                    <img src={Kinder}></img>
+                    <h5>Lorem</h5>
+                    <h6>$188</h6> 
+                    <div class="sale">
+                        <h4>Sale</h4>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="new" id="new">
+            <div class="centered-text">
+                <h2>Milk</h2>
+            </div>
+
+            <div class="new-content">
+                <div class="box">
+                    <img src={Kinder}></img>
+                    <h5>Lorem</h5>
+                    <h6>$188</h6> 
+                    <div class="sale">
+                        <h4>Sale</h4>
+                    </div>
+                </div>
+
+
+                <div class="box">
+                <img src={Kinder}></img>
+                    <h5>Lorem</h5>
+                    <h6>$188</h6> 
+                    <div class="sale">
+                        <h4>Sale</h4>
+                    </div>
+                </div>
+
+
+                <div class="box">
+                    <img src={Kinder}></img>
+                    <h5>Lorem</h5>
+                    <h6>$188</h6> 
+                    <div class="sale">
+                        <h4>Sale</h4>
+                    </div>
+                </div>
+
+
+                <div class="box">
+                    <img src={Kinder}></img>
+                    <h5>Lorem</h5>
+                    <h6>$188</h6> 
+                    <div class="sale">
+                        <h4>Sale</h4>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </>
+}
+
+export default ProductList;
--- src/pages/Products/index.js
+++ src/pages/Products/index.js
@@ -1,10 +1,12 @@
 import "./index.css"
 import Navbar from '../../components/Navbar'
+import ProductList from "./ProductList";
 
 
 const Products = () => {
     return <>
         <Navbar/>
+        <ProductList/>
     </>
 }
 
