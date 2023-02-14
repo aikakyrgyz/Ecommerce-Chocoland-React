a867e7cf938f165c661ac2146eac939c21f1f173
ayako54 <54380330+ayako54@users.noreply.github.com>
2023-02-14 00:36:21 -0800

    still working on order form to work

--- src/pages/ProductDetail/index.js
+++ src/pages/ProductDetail/index.js
@@ -1,11 +1,29 @@
-import "./index.css"
-import Navbar from '../../components/Navbar'
+import "./index.css";
+import Navbar from '../../components/Navbar';
+import OrderForm from "../../components/OrderForm";
+
+import { ReactDOM } from "react";
+
+
+import { useState } from 'react';
 
 
 const ProductDetail = () => {
-    return <>
-        <Navbar/>
+    
+    return (<>
+     <Navbar />
+     
+
+     <br/>
+     <br/>
+     <br/>
+     <br/>
+
+     <OrderForm />
+    
     </>
+    );
 }
+
 
 export default ProductDetail;
